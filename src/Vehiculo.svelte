cc39eb952fe83bddfd9aa88897ff83a3d6ffa956
Juan Carlos Brun <juancarlosbrun10@gmail.com>
2023-02-28 13:45:57 +0100

    CRUD Vehículos completo

---
+++ src/Vehiculo.svelte
@@ -0,0 +1,94 @@
+<script>
+    export let vehiculo = {
+        marca: "",
+        modelo: "",
+        version: "",
+        color: "",
+        anyo: 0,
+    };
+</script>
+
+<div class="card">
+    <div class="card-body">
+        <div class="row py-3">
+            <form>
+                <div class="col-12 row">
+                    <label class="col-form-label" for="marca"
+                        >Marca:
+                    </label><input
+                        id="marca"
+                        bind:value={vehiculo.marca}
+                        class="marca"
+                    />
+                </div>
+                <div class="col-12 row">
+                    <label class="col-form-label" for="modelo"
+                        >Modelo:
+                    </label><input
+                        id="modelo"
+                        bind:value={vehiculo.modelo}
+                        class="modelo"
+                    />
+                </div>
+                <div class="col-12 row">
+                    <label class="col-form-label" for="version"
+                        >Versión:
+                    </label><input
+                        id="version"
+                        bind:value={vehiculo.version}
+                        class="version"
+                    />
+                    <label class="col-form-label" for="color"
+                        >Color:
+                    </label><input
+                        id="color"
+                        bind:value={vehiculo.color}
+                        class="color"
+                    />
+                </div>
+                <div class="col-12 row">
+                    <label class="col-form-label" for="anyo">Año: </label><input
+                        id="anyo"
+                        bind:value={vehiculo.anyo}
+                        class="anyo"
+                    />
+                </div>
+            </form>
+        </div>
+        <slot />
+    </div>
+</div>
+
+<style>
+    .card {
+        background-color: transparent;
+        border: 2px solid white;
+        border-radius: 10px;
+        margin: 10px;
+    }
+    /*
+    .card:hover {
+        box-shadow: 0 8px 16px 0 rgba(0, 0, 0, 0.4);
+    }
+
+    input {
+        background-color: inherit;
+        padding: 10px;
+        font-size: 14px;
+        color: black;
+        border: 0;
+        outline: none;
+        margin-left: 5px;
+        margin-bottom: 4px;
+        max-width: 140px;
+    } */
+
+    /* input:focus {
+        background-color: wheat;
+    }
+
+    input:required:invalid,
+    input:focus:invalid {
+        background-color: lightpink;
+    } */
+</style>
--- src/Vehiculos.svelte
+++ src/Vehiculos.svelte
@@ -1,1 +1,57 @@
-<h1>Vehículos</h1>
+<script>
+    import { getContext, onMount } from "svelte";
+    import { jsonData } from "./store";
+    import Vehiculo from "./Vehiculo.svelte";
+    import Buscador from "./Buscador.svelte";
+    import Boton from "./Boton.svelte";
+
+    const URL = getContext("URL");
+    let busqueda = "";
+    let vehiculo = {};
+
+    onMount(async () => {
+        const response = await fetch(URL.vehiculos);
+        const data = await response.json();
+        $jsonData = data;
+    });
+
+    $: vehiculos = $jsonData.filter((item) =>
+        RegExp(busqueda, "i").test(item.marca)
+    );
+</script>
+
+<div class="d-flex justify-content-end">
+    <Buscador bind:busqueda />
+</div>
+<h2 class="text-center">Insertar vehículo:</h2>
+<div class="card-group row mx-0">
+    <div class="col-sm-6 col-md-4 col-lg-3 mx-auto">
+        <Vehiculo {vehiculo}>
+            <Boton documento={vehiculo} tipo="insertar" coleccion="vehiculos" />
+        </Vehiculo>
+    </div>
+</div>
+<h2 class="text-center mt-5">Colección de vehículos:</h2>
+
+<div class="vehiculos-container">
+    <div class="card-group row mx-0">
+        {#each vehiculos as vehiculo}
+            <div class="col-sm-6 col-md-4 col-lg-3 mx-0">
+                <Vehiculo {vehiculo}>
+                    <div class="row">
+                        <Boton
+                            documento={vehiculo}
+                            tipo="modificar"
+                            coleccion="vehiculos"
+                        />
+                        <Boton
+                            documento={vehiculo}
+                            tipo="eliminar"
+                            coleccion="vehiculos"
+                        />
+                    </div>
+                </Vehiculo>
+            </div>
+        {/each}
+    </div>
+</div>
